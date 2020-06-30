79a1cf5c787f3489c9bdca5e397a346fcb7d038c
Florian Rampin <contact@florianrampin.fr>
2020-07-01 00:09:08 +0200

    clean: adminDashboard+ AddadminArticleController

---
+++ src/Controller/Admin/ArticleController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Article;
+use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
+use phpDocumentor\Reflection\Types\This;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("admin/article")
+ */
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/new", name="admin_article_new", methods={"GET","POST"})
+     * @param Request $request
+     * @return Response
+     */
+    public function new(Request $request): Response
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article->setUser($this->getUser());
+
+            if (!$form->get('isAlert')->getData())
+            {
+                $article->setAlert(null);
+            }
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('article_index');
+        }
+
+        return $this->render('article/new.html.twig', [
+            'article' => $article,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="admin_article_edit", methods={"GET","POST"})
+     * @param Request $request
+     * @param Article $article
+     * @return Response
+     */
+    public function edit(Request $request, Article $article): Response
+    {
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('article_index');
+        }
+
+        return $this->render('article/edit.html.twig', [
+            'article' => $article,
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+    /**
+     * @Route("/{id}", name="admin_article_delete", methods={"DELETE"})
+     * @param Request $request
+     * @param Article $article
+     * @return Response
+     */
+    public function delete(Request $request, Article $article): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$article->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($article);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('article_index');
+    }
+}
--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,40 +2,18 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\Area;
-use App\Entity\Article;
-use App\Entity\Category;
-use App\Entity\Site;
-use App\Entity\User;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DashboardController extends AbstractDashboardController
+class DashboardController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/admin/dashboard", name="admin_dashboard_index")
      */
-    public function index(): Response
+    public function index()
     {
-        return parent::index();
-    }
-
-    public function configureDashboard(): Dashboard
-    {
-        return Dashboard::new()
-            ->setTitle('PortLaMontagne');
-    }
-
-    public function configureMenuItems(): iterable
-    {
-        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Article', 'icon class', Article::class);
-        yield MenuItem::linkToCrud('Area', 'icon class', Area::class);
-        yield MenuItem::linkToCrud('Category', 'icon class', Category::class);
-        yield MenuItem::linkToCrud('Site', 'icon class', Site::class);
-        yield MenuItem::linkToCrud('User', 'icon class', User::class);
+        return $this->render('admin/dashboard/index.html.twig', [
+            'controller_name' => 'DashboardController',
+        ]);
     }
 }
