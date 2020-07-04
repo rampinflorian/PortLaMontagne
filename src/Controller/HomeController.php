0d34a843bbf38f0ae7702cbf6b56b7d3c43511b9
rampinflorian <contact@florianrampin.fr>
2020-07-04 12:19:35 +0200

    Finish homePage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\AlertRepository;
 use App\Repository\ArticleRepository;
+use App\Repository\ClimbingGroupRepository;
 use App\Repository\PartnerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
@@ -16,13 +17,16 @@ class HomeController extends AbstractController
      * @Route("/", name="home")
      * @param ArticleRepository $articleRepository
      * @param PartnerRepository $partnerRepository
+     * @param ClimbingGroupRepository $climbingGroupRepository
      * @param ParameterBagInterface $parameterBag
      * @return Response
      */
-    public function index(ArticleRepository $articleRepository, PartnerRepository $partnerRepository, ParameterBagInterface $parameterBag)
+    public function index(ArticleRepository $articleRepository, PartnerRepository $partnerRepository, ClimbingGroupRepository $climbingGroupRepository, ParameterBagInterface $parameterBag)
     {
         return $this->render('home/index.html.twig', [
+            'countArticles' => count($articleRepository->findAll()),
             'articles' => $articleRepository->findAllWithMaxResult(4),
+            'countGroups' => count($climbingGroupRepository->findAll()),
             'partners' => $partnerRepository->findAll(),
             'partners_directory' => $parameterBag->get('partner_directory')
         ]);
