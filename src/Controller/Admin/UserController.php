11eb7ae41e354591b01c9649ceff94b3b955b598
rampinflorian <contact@florianrampin.fr>
2020-07-10 20:46:21 +0200

    Add: Admin users list

---
+++ src/Controller/Admin/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("admin/user", name="admin_user_index")
+     * @param UserRepository $userRepository
+     * @return Response
+     */
+    public function index(UserRepository $userRepository)
+    {
+
+
+        return $this->render('admin/user/index.html.twig', [
+        'users' => $userRepository->findAll()
+        ]);
+    }
+}
