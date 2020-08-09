a27a97072fc3430f8ad81a6385a6ce518f0af972
rampinflorian <contact@florianrampin.fr>
2020-08-09 14:50:28 +0200

    Update DefaultController.php

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,6 +7,11 @@ use App\Service\CallWeatherAPIService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use  Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class DefaultController extends AbstractController
 {
@@ -26,10 +31,15 @@ class DefaultController extends AbstractController
     /**
      * @Route("/default/open-weather-map", name="default_open-weather-map")
      * @param CallWeatherAPIService $callWeatherAPIService
+     * @return Response
+     * @throws ClientExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
      */
     public function openWeatherMapAction(CallWeatherAPIService $callWeatherAPIService) : Response
     {
-
 
         return $this->json([
             'code' => 200,
