ab298265cdb13fa870d31da59614c1fe792af3ff
Florian Rampin <contact@florianrampin.fr>
2020-09-07 22:21:59 +0200

    fix: Fixutres objectManager

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,7 +7,7 @@ use App\Entity\Category;
 use App\Entity\Topo;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 
 class AppFixtures extends Fixture
